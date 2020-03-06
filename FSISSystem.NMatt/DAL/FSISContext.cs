70ba4986b5fe95b10350d41bb7027be26e602b1c
Nathan Matt <nmatt1@nait.ca>
2020-03-06 11:19:06 -0700

    Add project files.

---
+++ FSISSystem.NMatt/DAL/FSISContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using FSISSystem.NMatt;
+
+
+namespace FSISSystem.NMatt.DAL
+{
+    internal class FSISContext : DbContext
+    {
+        public FSISContext() : base("")
+        {
+        }
+        public DbSet<Region> Regions { get; set; }
+        
+    }
+    
+}
---
+++ FSISSystem.NMatt/Region.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FSISSystem.NMatt
+{
+    [Table("Region")]
+    class Region
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RegionID { get; set; }
+        public string RegionDescription { get; set; }
+    }
+}
